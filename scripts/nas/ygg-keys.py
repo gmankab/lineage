60447ed601c0931df161eaaa96816403564d8618
gmanka <gmankab@gmail.com>
2025-10-19 03:19:48 +0500

    [u] store private key in secrets

--- scripts/nas/ygg-keys.py
+++ scripts/nas/ygg-keys.py
@@ -1,7 +1,7 @@
 '''
-1. generates yggdrasil config
+1. generates yggdrasil configs
 2. gets if from this configs
-3. writes privatekey and ip to github output
+3. writes privatekey to repo secrets and ip to repo variables
 '''
 
 import subprocess
@@ -12,12 +12,14 @@ from pathlib import Path
 
 
 class common:
-    github_output: Path = Path(
-        os.getenv('GITHUB_OUTPUT', 'test_github_output.env')
-    )
-    __yggdrasil__ = shutil.which('yggdrasil')
-    assert __yggdrasil__
-    yggdrasil: Path = Path(__yggdrasil__)
+    repo: str = os.getenv('GITHUB_REPOSITORY', '')
+    assert repo
+    _yggdrasil = shutil.which('yggdrasil')
+    assert _yggdrasil
+    yggdrasil: Path = Path(_yggdrasil)
+    _gh = shutil.which('gh')
+    assert _gh
+    gh: Path = Path(_gh)
 
 
 def get_ip(private_key: str) -> str:
@@ -37,18 +39,27 @@ def generate_private_key() -> str:
     )['PrivateKey'].strip()
 
 
-def write_output(prefix: str):
+def write_secrets(prefix: str):
     private_key = generate_private_key()
     ip = get_ip(private_key)
-    with common.github_output.open('a') as output:
-        output.write(f'{prefix}_privatekey={private_key}\n')
-        output.write(f'{prefix}_ip={ip}\n')
+    subprocess.run(
+        args=['gh', 'secret', 'set', '--repo', common.repo, f'{prefix}_privatekey'],
+        input=private_key,
+        check=True,
+        text=True,
+    )
+    subprocess.run(
+        args=['gh', 'variable', 'set', '--repo', common.repo, f'{prefix}_ip'],
+        input=ip,
+        check=True,
+        text=True,
+    )
 
 
 def main():
-    write_output('builder')
-    write_output('repo')
-    write_output('out')
+    write_secrets('builder')
+    write_secrets('repo')
+    write_secrets('out')
 
 
 main()
